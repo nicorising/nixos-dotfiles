0ab2603fb937bbe9e4f249c8e80777523c64777e
Nico Rising <nico@nicorising.com>
2025-10-11 01:29:32 -0400

    Hyprsunset config

--- programs/hyprsunset.nix
+++ programs/hyprsunset.nix
@@ -2,27 +2,32 @@
   services.hyprsunset = {
     enable = true;
 
-    transitions = {
-      sunrise = {
-        calendar = "*-*-* 06:00:00";
-        requests = [
-          [
-            "temperature"
-            "6500"
-          ]
-          [ "gamma 100" ]
-        ];
-      };
-
-      sunset = {
-        calendar = "*-*-* 19:00:00";
-        requests = [
-          [
-            "temperature"
-            "3500"
-          ]
-        ];
-      };
-    };
+    settings.profile = [
+      {
+        time = "6:00";
+        temperature = 6500;
+        gamma = 1.0;
+      }
+      {
+        time = "19:00";
+        temperature = 5000;
+        gamma = 1.0;
+      }
+      {
+        time = "20:00";
+        temperature = 4000;
+        gamma = 0.95;
+      }
+      {
+        time = "21:00";
+        temperature = 3000;
+        gamma = 0.9;
+      }
+      {
+        time = "22:00";
+        temperature = 2700;
+        gamma = 0.85;
+      }
+    ];
   };
 }
