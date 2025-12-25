6bca0531efeda72be2da8185df48c37d330b9193
Nico Rising <nico@nicorising.com>
2025-12-25 15:38:28 -0500

    Change description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My laptop's NixOS configuration files";
+  description = "My NixOS configuration files";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
