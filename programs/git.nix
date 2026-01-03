666f2be2314db4fc38baead98fa111d5635f0b8a
Nico Rising <nico@nicorising.com>
2026-01-03 13:28:23 -0500

    Upgrade packages

--- programs/git.nix
+++ programs/git.nix
@@ -2,28 +2,30 @@
   programs.git = {
     enable = true;
 
-    userName = "Nico Rising";
-    userEmail = "nico@nicorising.com";
-
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
-    };
 
-    aliases = {
-      lg = "log --graph --oneline --all";
-      pfwl = "push --force-with-lease";
-      history = ''
-        !f() {
-          i=0
-          while [ $i -lt 10 ]; do
-            i=$((i + 1))
-            ref=$(git rev-parse --symbolic-full-name @{-$i} 2> /dev/null) || break
-            printf "@{-%d} %s\n" "$i" "$ref"
-          done
-        }
-        f
-      '';
+      user = {
+        name = "Nico Rising";
+        email = "nico@nicorising.com";
+      };
+
+      aliases = {
+        lg = "log --graph --oneline --all";
+        pfwl = "push --force-with-lease";
+        history = ''
+          !f() {
+            i=0
+            while [ $i -lt 10 ]; do
+              i=$((i + 1))
+              ref=$(git rev-parse --symbolic-full-name @{-$i} 2> /dev/null) || break
+              printf "@{-%d} %s\n" "$i" "$ref"
+            done
+          }
+          f
+        '';
+      };
     };
   };
 }
